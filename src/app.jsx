a84c6a3651c688e3cb15b418bd4454d247af0982
allen79yu <allen.yu@siliconmotion.com.tw>
2018-07-29 13:26:57 +0800

    remove useless npm script and fix some config

--- src/app.jsx
+++ src/app.jsx
@@ -1,29 +1,32 @@
-import "@styles/app.scss";
+import '@styles/app.scss';
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import thunkMiddleware from "redux-thunk";
-import { createLogger } from "redux-logger";
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import reducers from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
 //main frame
-import MainFrame from "@components/MainFrame";
+import MainFrame from '@components/MainFrame';
 
 //redux loggers
 const loggerMiddleware = createLogger();
 
 //init redux devtool
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware)));
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware))
+);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router basename="/boilerplate/">
-            <MainFrame />
-        </Router>
-    </Provider>,
-    document.getElementById("app")
+  <Provider store={store}>
+    <Router>
+      <MainFrame />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
 );
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,53 +1,41 @@
-const path = require("path");
-const webpack = require("webpack");
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: `${__dirname}/src/index.html`,
-    filename: "index.html",
-    inject: "body",
-    favicon: "./src/favicon.ico"
+  template: `${__dirname}/src/index.html`,
+  filename: 'index.html',
+  inject: 'body',
+  favicon: './src/favicon.ico'
 });
 
-let apiHost = () => {
-    switch (process.env.NODE_ENV) {
-        case "localDev":
-            return "'http://localhost:8000/apis/'";
-            break;
-        case "dev":
-            return "'https://stage.host/apis/'";
-            break;
-        case "production":
-            return "'https://prod.host/apis/'";
-            break;
-    }
-};
-
 module.exports = merge(common, {
-    plugins: [
-        HTMLWebpackPluginConfig,
-        new webpack.DefinePlugin({
-            "process.env": {
-                NODE_ENV: JSON.stringify("production")
-            },
-            __API__: apiHost()
-        }),
-        new webpack.NoEmitOnErrorsPlugin(),
-        new ExtractTextPlugin("assets/[name].[hash].css"),
-        new webpack.optimize.CommonsChunkPlugin({ name: "vendors", filename: "assets/[name].[hash].js" }),
-        new webpack.optimize.UglifyJsPlugin({
-            beautify: false,
-            mangle: {
-                screw_ie8: true,
-                keep_fnames: true
-            },
-            compress: {
-                screw_ie8: true
-            },
-            comments: false
-        })
-    ]
+  plugins: [
+    HTMLWebpackPluginConfig,
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new ExtractTextPlugin('assets/[name].[hash].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendors',
+      filename: 'assets/[name].[hash].js'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      comments: false
+    })
+  ]
 });
